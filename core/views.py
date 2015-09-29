8d8cb171948d3f628b33b3d3191f63bcef3985b6
Bill <wfhorobinjr@quinnipiac.edu>
2015-09-29 18:31:12 +0000

    Add question list view

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
+
 
 # Create your views here.
 class Home(TemplateView):
@@ -11,8 +12,12 @@ class QuestionCreateView(CreateView):
   model = Question
   template_name = "question/question_form.html"
   fields = ['title', 'description']
-  success_url = reverse_lazy('home')
+  success_url = reverse_lazy('question_list')
 
   def form_valid(self, form):
     form.instance.user = self.request.user
     return super(QuestionCreateView, self).form_valid(form)
+
+class QuestionListView(ListView):
+  model = Question
+  template_name = "question/question_list.html"
--- qandu_app/settings.py
+++ qandu_app/settings.py
@@ -116,5 +116,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(MAIN_DIR, 'static'),)
 STATIC_ROOT = 'staticfiles'
 LOGIN_URL = '/user/login'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/question/'
 SITE_ID = 1
