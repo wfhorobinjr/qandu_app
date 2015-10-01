3ef68ce3de37d93610ccc1bb5c2a14d1cd966c1a
Bill <wfhorobinjr@quinnipiac.edu>
2015-10-01 18:14:26 +0000

    Add answer create view

--- core/views.py
+++ core/views.py
@@ -35,3 +35,16 @@ class QuestionDeleteView(DeleteView):
   model = Question
   template_name = 'question/question_confirm_delete.html'
   success_url = reverse_lazy('question_list')
+
+class AnswerCreateView(CreateView):
+  model = Answer
+  template_name = "answer/answer_form.html"
+  fields = ['text']
+
+  def get_success_url(self):
+    return self.object.question.get_absolute_url()
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    form.instance.question = Question.objects.get(id=self.kwargs['pk'])
+    return super(AnswerCreateView, self).form_valid(form)
