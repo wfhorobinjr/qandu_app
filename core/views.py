304d7f225d7e843ba168bde3fc19648d13529560
Bill <wfhorobinjr@quinnipiac.edu>
2015-10-13 18:48:43 +0000

    Add search

--- core/views.py
+++ core/views.py
@@ -177,3 +177,8 @@ class UserDeleteView(DeleteView):
     user.is_active = False
     user.save()
     return redirect(self.get_success_url())
+
+class SearchQuestionListView(QuestionListView):
+  def get_queryset(self):
+    incoming_query_string = self.request.GET.get('query','')
+    return Question.objects.filter(title__icontains=incoming_query_string)
